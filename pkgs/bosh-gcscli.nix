63554b4901f9975cad5dc455690f990c1d33bacd
rkoster <hi@rkoster.dev>
2026-07-07 10:00:03 +0200

    feat(m3): source-built bosh-gcscli

--- pkgs/bosh-gcscli.nix
+++ pkgs/bosh-gcscli.nix
@@ -3,7 +3,7 @@ callPackage ../lib/mk-blobstore-cli.nix { } {
   pname = "bosh-gcscli";
   version = "0.0.393";
   repo = "bosh-gcscli";
-  hash = lib.fakeHash;
-  vendorHash = lib.fakeHash;
+  hash = "sha256-LwsfF7OAweJBjzvilC5dpkWAnC3dAKgINlDk7Jf//pU=";
+  vendorHash = null;  # bosh-gcscli vendors its Go dependencies
   ldflagsVersionVar = null;
 }
